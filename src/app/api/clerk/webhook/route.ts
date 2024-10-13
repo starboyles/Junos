da0a54dcdd0b46b05572e7262ab2e5a330f10824
Kwame Sarfo <lesliegyamfi02@gmail.com>
2024-10-13 15:05:14 +0000

    Refactor webhook route to create user from webhook data

--- src/app/api/clerk/webhook/route.ts
+++ src/app/api/clerk/webhook/route.ts
@@ -1,23 +1,23 @@
 import { db } from "@/server/db";
 
 export const POST = async (req: Request) => {
-  const { data } = await req.json();
-  console.log("webhook received", data);
-  const emailAddress = data.email_addresses[0].email_address;
-  const firstName = data.first_name;
-  const lastName = data.last_name;
-  const imageUrl = data.image_url;
-  const id = data.id;
+    const { data } = await req.json();
+    console.log("webhook received", data);
+    const emailAddress = data.email_addresses[0].email_address;
+    const firstName = data.first_name;
+    const lastName = data.last_name;
+    const imageUrl = data.image_url;
+    const id = data.id;
 
-  await db.user.create({
-    data: {
-          id: id,
-          firstName: firstName,
-          lastName: lastName,
-          imageUrl: imageUrl, 
-          emailAddress: emailAddress, 
+    await db.user.create({
+        data: {
+            id: id,
+            firstName: firstName,
+            lastName: lastName,
+            imageUrl: imageUrl,
+            emailAddress: emailAddress,
         },
-  });
-
-  return new Response("webhook received", { status: 200 });
+    });
+    console.log("user successfully created");
+    return new Response("webhook received", { status: 200 });
 };

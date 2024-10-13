23bc113642cf08b7063cefecb04e85ad23db0f4a
Kwame Sarfo <lesliegyamfi02@gmail.com>
2024-10-13 15:08:41 +0000

    remove console.log stmt

--- src/app/api/clerk/webhook/route.ts
+++ src/app/api/clerk/webhook/route.ts
@@ -2,7 +2,6 @@ import { db } from "@/server/db";
 
 export const POST = async (req: Request) => {
     const { data } = await req.json();
-    console.log("webhook received", data);
     const emailAddress = data.email_addresses[0].email_address;
     const firstName = data.first_name;
     const lastName = data.last_name;

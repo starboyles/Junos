6800f10638b52155d3d46a3ae73f7950b50aac12
Kwame Sarfo <lesliegyamfi02@gmail.com>
2024-10-13 23:39:55 +0000

    aurinnko auth setup

--- src/lib/aurinko.ts
+++ src/lib/aurinko.ts
@@ -1,12 +1,22 @@
-"use server"
+"use server";
 
-import { auth } from "@clerk/nextjs/server"
+import { auth } from "@clerk/nextjs/server";
 
-export const getAurinkoAuthUrl = async (serviceType: 'Google' | 'Office 365') => {
-    const { userId } = await auth()
-    if (!userId) {
-        throw new Error('User not authenticated')
-    }
+export const getAurinkoAuthUrl = async (
+  serviceType: "Google" | "Office 365",
+) => {
+  const { userId } = await auth();
+  if (!userId) {
+    throw new Error("User not authenticated");
+  }
+  const params = new URLSearchParams({
+    clientId: process.env.AURINKO_CLIENT_ID as string,
+    serviceType,
+    scopes: "Mail.Read Mail.ReadWrite Mail.Send Mail.Drafts Mail.All",
+    responseType: "code",
+    returnUrl: `${process.env.NEXT_PUBLIC_URL}/api/aurinko/callback`,
+  });
 
+  return `https://api.aurinko.io/v1/auth/authorize?${params.toString()}`
 
-}
+};

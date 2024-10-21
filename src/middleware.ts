add478b4a1adb583266e75b1eadee088bb61f5b8
Kwame Sarfo <lesliegyamfi02@gmail.com>
2024-10-21 13:56:43 +0000

    Add '/api/initial-sync' route to public routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)','/api/clerk/webhook(.*)'])
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)','/api/clerk/webhook(.*)', '/api/initial-sync(.*)'])
 
 export default clerkMiddleware((auth, request) => {
   if (!isPublicRoute(request)) {
